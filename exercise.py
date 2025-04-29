7da75f87c58660746a1701ceb836c7b3d4991288
kisapon <varyamerkulovaaa@gmail.com>
2025-04-29 17:43:31 +0700

    2_1

--- exercise.py
+++ exercise.py
@@ -1,8 +1,30 @@
-nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
-def quantity(nums):
-    for i in nums:
-        counter = nums.count(i)
-        if counter >= 2:
-            return True
-        return False
-print(quantity(nums))
+class Student:
+    def __init__(self, surname, birth, group, rating):
+        self.surname = surname
+        self.birth = birth
+        self.group = group
+        self.rating = rating
+    def change_surname(self, input_surname):
+        self.surname = input_surname
+    def change_birth(self, input_birth):
+        self.birth = input_birth
+    def change_group(self, input_group):
+        self.group = input_group
+    def print_info(self):
+        print("Фамилия:", self.surname)
+        print("Дата рождения:", self.birth)
+        print("Группа:", self.group)
+        print("Успеваемость:", self.rating)
+student = Student("Иванов", "01.01.2007", 632, [3, 5, 2, 4, 5])
+print("Информация о студенте:")
+student.print_info()
+que1 = input("Хотите изменить фамилию? да/нет: ")
+if que1 == "да":
+    input_surname = input("Введите фамилию: ")
+    student.change_surname(input_surname)
+que2 = input("Хотите изменить дату рождения? да/нет: ")
+if que2 == "да":
+    input_birth = input("Введите дату рождения: ")
+    student.change_birth(input_birth)
+print("\nИнформация о студенте после изменений:")
+student.print_info()
