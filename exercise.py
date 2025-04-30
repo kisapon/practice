4954d4bd0c2a029d92607fb68c8947da834f80fa
kisapon <varyamerkulovaaa@gmail.com>
2025-04-30 12:20:38 +0700

    2_3

--- exercise.py
+++ exercise.py
@@ -1,22 +1,19 @@
-class Train:
-    def __init__(self, name, number, time):
-        self.name = name
-        self.number = number
-        self.time = time
+class Numbers:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
     def print_info(self):
-        print("Пункт назначения: ", self.name)
-        print("Номер поезда : ", self.number)
-        print("Время отправления: ", self.time)
-train1 = Train("Томск", 112, "21:10")
-train2 = Train("Москва", 331, "08:30")
-train3 = Train("Пермь", 123, "17:50")
-trains = [train1, train2, train3]
-num = input("Введите номер поезда: ")
-found = False
-for i in trains:
-    if i.number == int(num):
-        i.print_info()
-        found = True
-        break
-if not found:
-    print("Нет поезда с таким номером")
+        print("Первое число: ", self.num1)
+        print("Второе число: ", self.num2)
+    def sum_even(self):
+        summ = self.num1 + self.num2
+        print("Сумма чисел: ", summ)
+    def maximum_num(self):
+        max_num = max(self.num1, self.num2)
+        print("Наибольшее число: ", max_num)
+        if self.num1 == self.num2:
+            print("Числа равны")
+numbers = Numbers(5, 2)
+numbers.print_info()
+numbers.sum_even()
+numbers.maximum_num()
