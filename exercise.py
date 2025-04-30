272dd41856e2a600268dbe86ce573f05780895a7
kisapon <varyamerkulovaaa@gmail.com>
2025-04-30 09:27:37 +0700

    2_2

--- exercise.py
+++ exercise.py
@@ -1,29 +1,22 @@
-class Student:
-    def __init__(self, surname, birth, group, rating):
-        self.surname = surname
-        self.birth = birth
-        self.group = group
-        self.rating = rating
-    def change_surname(self, input_surname):
-        self.surname = input_surname
-    def change_birth(self, input_birth):
-        self.birth = input_birth
-    def change_group(self, input_group):
-        self.group = input_group
+class Train:
+    def __init__(self, name, number, time):
+        self.name = name
+        self.number = number
+        self.time = time
     def print_info(self):
-        print("Фамилия:", self.surname)
-        print("Дата рождения:", self.birth)
-        print("Группа:", self.group)
-        print("Успеваемость:", self.rating)
-student = Student("Иванов", "01.01.2007", 632, [3, 5, 2, 4, 5])
-print("Информация о студенте:")
-student.print_info()
-print()
-input_surname = input("Введите фамилию: ")
-student.change_surname(input_surname)
-input_birth = input("Введите дату рождения: ")
-student.change_birth(input_birth)
-input_group = input("Введите группу: ")
-student.change_group(input_group)
-print("\nИнформация о студенте после изменений:")
-student.print_info()
+        print("Пункт назначения: ", self.name)
+        print("Номер поезда : ", self.number)
+        print("Время отправления: ", self.time)
+train1 = Train("Томск", 112, "21:10")
+train2 = Train("Москва", 331, "08:30")
+train3 = Train("Пермь", 123, "17:50")
+trains = [train1, train2, train3]
+num = input("Введите номер поезда: ")
+found = False
+for i in trains:
+    if i.number == int(num):
+        i.print_info()
+        found = True
+        break
+if not found:
+    print("Нет поезда с таким номером")
