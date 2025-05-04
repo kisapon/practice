6c0f91a4af4509c7d8c9ba2ce2edfc9ab7c1d7c2
kisapon <varyamerkulovaaa@gmail.com>
2025-05-04 20:11:29 +0700

    3_2

--- exercise.py
+++ exercise.py
@@ -1,11 +1,20 @@
 class Worker:
     def __init__(self, name, surname, rate, days):
-        self.name = name
-        self.surname = surname
-        self.rate = rate
-        self.days = days
+        self.__name = name
+        self.__surname = surname
+        self.__rate = rate
+        self.__days = days
+    def GetName(self):
+        return self.__name
+    def GetSurname(self):
+        return self.__surname
+    def GetRate(self):
+        return self.__rate
+    def GetDays(self):
+        return self.__days
     def GetSalary(self):
-        salary = self.rate * self.days
+        salary = self.__rate * self.__days
         print("Зарплата работника: ", salary)
+        return salary
 worker = Worker("Иван", "Иванов", 1000, 15)
 worker.GetSalary()
