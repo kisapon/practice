44fe33ce53fa772261f7721a7e574433d0b1ad41
kisapon <varyamerkulovaaa@gmail.com>
2025-05-13 03:51:32 +0700

    1_2

--- exercise.py
+++ exercise.py
@@ -1,21 +1,19 @@
-class Class:
-    def __init__(self, attribute1=None, attribute2=None):
-        self.attribute1 = attribute1
-        self.attribute2 = attribute2
-        print("Объект создан с значениями:", self.attribute1, "и", self.attribute2)
-
-    def __del__(self):
-        print("Объект с", self.attribute1, "и", self.attribute2,"удалён")
-
-#демонстрация ввозможностей класса
-object1 = Class("значение1", "значение2")
-object2 = Class() #значения по умолчанию
-object1.attribute1 = "новое значение1"
-object1.attribute2 = "новое значение2"
-print("Новые объекты:", object1.attribute1, "и", object1.attribute2)
-del object1
-del object2
-
+candidates = [2, 5, 2, 1, 2]
+target = 5
+def backtrack(start, path, target):
+    if target == 0:
+        result.append(path)
+        return
+    if target < 0:
+        return
+    for i in range(start, len(candidates)):
+        if i > start and candidates[i] == candidates[i - 1]:
+            continue
+        backtrack(i + 1, path + [candidates[i]], target - candidates[i])
+candidates.sort()
+result = []
+backtrack(0, [], target)
+print(result)
 
 
 
