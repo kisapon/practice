f323b6b1a1c3ec9aa43882c0f9d0e7b36266d9b6
kisapon <varyamerkulovaaa@gmail.com>
2025-05-13 02:12:20 +0700

    4_1_right

--- exercise.py
+++ exercise.py
@@ -9,9 +9,12 @@ class Students:
         self.group = group
         self.rating = rating
 
-con = sqlite3.connect("StudentsDB.db")
+    def calculate_average(self):
+        return round(sum(self.rating) / len(self.rating), 2)
+
+con = sqlite3.connect("StudentsDataBase.db")
 cursor = con.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS StudentsDB (
+cursor.execute("""CREATE TABLE IF NOT EXISTS StudentsDataBase (
 ID INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT,
 surname TEXT,
@@ -24,92 +27,135 @@ rate4 INTEGER
 )""")
 con.commit()
 
-def add_student():
-    name = input("Введите имя: ")
-    surname = input("Введите фамилию: ")
-    patronymic = input("Введите отчество: ")
-    group = input("Введите группу: ")
-    rating = []
-    for i in range(1, 5):
-        rate = int(input(f"Введите {i}-ую оценку: "))
-        rating.append(rate)
-    cursor.execute("""INSERT INTO StudentsDB (name, surname, patronymic, group_name, rate1, rate2, rate3, rate4) VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
-(name, surname, patronymic, group, rating[0], rating[1], rating[2], rating[3]))
+def add_student(student):
+    cursor.execute(
+        """INSERT INTO StudentsDataBase (name, surname, patronymic, group_name, rate1, rate2, rate3, rate4) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+        (student.name, student.surname, student.patronymic, student.group,
+         student.rating[0], student.rating[1], student.rating[2], student.rating[3]))
     con.commit()
     st_id = cursor.lastrowid
-    print("Студент добавлен. Его ID:", st_id)
+    return st_id
 
 def view_all_students():
-    cursor.execute("""SELECT * FROM StudentsDB""")
+    cursor.execute("""SELECT * FROM StudentsDataBase""")
     items = cursor.fetchall()
+    students_data = []
     for item in items:
-        print(f"ID: {item[0]}\tИмя: {item[1]}\tФамилия: {item[2]}\tОтчество: {item[3]}"
-              f"\tГруппа: {item[4]} \tОценки: [{item[5]}, {item[6]}, {item[7]}, {item[8]}]")
+        students_data.append(item)
+    return students_data
 
-def view_student():
-    student_id = input("Введите ID студента: ")
-    cursor.execute("""SELECT * FROM StudentsDB WHERE id = ?""", (student_id,))
+def view_student(st_id):
+    cursor.execute("""SELECT * FROM StudentsDataBase WHERE id = ?""", (st_id,))
     item = cursor.fetchone()
     if item:
-        average = round(((float(item[5]) + float(item[6]) + float(item[7]) + float(item[8])) / 4), 2)
-        print(f"ID: {item[0]}\tИмя: {item[1]}\tФамилия: {item[2]}\tОтчество: {item[3]}"
-              f"\tГруппа: {item[4]}\tОценки: [{item[5]}, {item[6]}, {item[7]}, {item[8]}]\t Средний балл: {average}")
+        return item
     else:
-        print("Студент с таким ID не найден.")
+        return None
 
-def editing_student():
-    student_id = input("Введите ID студента: ")
-    cursor.execute("""SELECT * FROM StudentsDB WHERE id = ?""", (student_id,))
+def editing_student(st_id, student):
+    cursor.execute("""UPDATE StudentsDataBase SET name = ?, surname = ?, patronymic = ?, group_name = ?, 
+                        rate1 = ?, rate2 = ?, rate3 = ?, rate4 = ? WHERE id = ?""",
+                       (student.name, student.surname, student.patronymic, student.group, student.rating[0],
+                        student.rating[1], student.rating[2], student.rating[3], st_id))
+    con.commit()
+
+def delete_student(st_id):
+    cursor.execute("""SELECT * FROM StudentsDataBase WHERE id = ?""", (st_id,))
     item = cursor.fetchone()
     if item:
-        name = input("Введите новое имя: ")
-        surname = input("Введите новую фамилию: ")
-        patronymic = input("Введите новое отчество: ")
-        group = input("Введите новую группу: ")
-        rating = []
-        for i in range(1, 5):
-            rate = int(input(f"Введите новую {i}-ую оценку: "))
-            rating.append(rate)
-        cursor.execute("""UPDATE StudentsDB SET name = ?, surname = ?, patronymic = ?, group_name = ?, 
-                    rate1 = ?, rate2 = ?, rate3 = ?, rate4 = ? WHERE id = ?""",
-        (name, surname, patronymic, group, rating[0], rating[1], rating[2], rating[3], student_id))
+        cursor.execute("DELETE FROM StudentsDataBase WHERE id = ?", (st_id,))
         con.commit()
-        print("Студент изменен.")
-    else:
-        print("Студент с таким ID не найден.")
-
-def delete_student():
-    student_id = input("Введите ID студента: ")
-    cursor.execute("""SELECT * FROM StudentsDB WHERE id = ?""", (student_id,))
-    item = cursor.fetchone()
-    if item:
-        cursor.execute("DELETE FROM StudentsDB WHERE id = ?", (student_id,))
         print("Студент удален.")
     else:
         print("Студент с таким ID не найден.")
-    con.commit()
 
-# def average_rating():
+def group_average(group_name):
+    cursor.execute("""SELECT AVG((rate1 + rate2 + rate3 + rate4) / 4.0) FROM StudentsDataBase WHERE group_name = ?""", (group_name,))
+    result = cursor.fetchone()
+    if result and result[0] is not None:
+        print("Средний балл студентов этой группы: ", result[0])
+    else:
+        print("Группа не найдена или в ней нет студентов.")
 
 print("Приложение по работе со студентами.")
 while True:
     print("Функционал:")
     print(f"1 -> Добавление нового студента. \n2 -> Просмотр всех студентов."
           f" \n3 -> Просмотр одного студента, включая его средний балл. \n4 -> Редактирование студента."
-          f"\n5 -> Удаление студента. \n6 -> Просмотр среднего балла студентов у конкретной группы. \n7 -> Выход.")
-    choice = input("Выберите функцию: ")
-    if choice == '1':
-        add_student()
-    elif choice == '2':
-        view_all_students()
-    elif choice == '3':
-        view_student()
-    elif choice == '4':
-        editing_student()
-    elif choice == '5':
-        delete_student()
-    elif choice == '7':
-        con.close()
-        break
-    else:
-        print("Такой функции нет! Попробуйте снова.")
+          f"\n5 -> Удаление студента. \n6 -> Просмотр среднего балла студентов у конкретной группы. \n7 -> Выход.\n")
+    try:
+        choice = int(input("Выберите функцию: "))
+        if choice == 1:
+            name = input("Введите имя: ")
+            surname = input("Введите фамилию: ")
+            patronymic = input("Введите отчество: ")
+            group = input("Введите группу: ")
+            rating = []
+            for i in range(1, 5):
+                try:
+                    rate = int(input(f"Введите новую {i}-ую оценку: "))
+                    rating.append(rate)
+                except ValueError:
+                    print("Введите целое число.")
+            student = Students(name, surname, patronymic, group, rating)
+            st_id = add_student(student)
+            print("Студент добавлен. Его ID:", st_id)
+
+        elif choice == 2:
+            students_data = view_all_students()
+            if students_data:
+                for item in students_data:
+                    student = Students(item[1], item[2], item[3], item[4], [item[5], item[6], item[7], item[8]])
+                    print(
+                        f"ID: {item[0]}\tИмя: {item[1]}\tФамилия: {item[2]}\tОтчество: {item[3]}"
+                        f"\tГруппа: {item[4]} \tОценки: [{item[5]}, {item[6]}, {item[7]}, {item[8]}] \tСредний балл: {student.calculate_average()}") #выводим через item а не student
+            else:
+                print("Нет данных о студентах.")
+
+        elif choice == 3:
+            st_id = int(input("Введите ID: "))
+            item = view_student(st_id)
+            if item:
+                student = Students(item[1], item[2], item[3], item[4], [item[5], item[6], item[7], item[8]])
+                print(
+                    f"ID: {item[0]}\tИмя: {item[1]}\tФамилия: {item[2]}\tОтчество: {item[3]}"
+                    f"\tГруппа: {item[4]} \tОценки: {item[5]}, {item[6]}, {item[7]}, {item[8]} \tСредний балл: {student.calculate_average()}") #выводим через item а не student
+            else:
+                print("Студент с таким ID не найден.")
+
+        elif choice == 4:
+            st_id = int(input("Введите ID: "))
+            ex_student_item = view_student(st_id)
+            if ex_student_item:
+                name = input("Введите новое имя: ")
+                surname = input("Введите новую фамилию: ")
+                patronymic = input("Введите новое отчество: ")
+                group = input("Введите новую группу: ")
+                rating = []
+                for i in range(1, 5):
+                    try:
+                        rate = int(input(f"Введите новую {i}-ую оценку: "))
+                        rating.append(rate)
+                    except ValueError:
+                        print("Введите целое число.")
+                student = Students(name, surname, patronymic, group, rating)
+                editing_student(st_id, student)
+                print("Студент изменен.")
+            else:
+                print("Студент с таким ID не найден.")
+
+        elif choice == 5:
+            st_id = int(input("Введите ID: "))
+            delete_student(st_id)
+
+        elif choice == 6:
+            group_name = input("Введите группу: ")
+            group_average(group_name)
+        elif choice == 7:
+            con.close()
+            break
+        else:
+            print("Такой функции нет! Попробуйте снова.")
+    except ValueError:
+        print("Номер функции - целое число!")
